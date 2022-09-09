86ff602c3b9d45a8a0d3d1ee1fd89cda0bf105b4
mkorovaynyy <corowainy@yandex.ru>
2022-09-09 12:32:24 +0300

    корректировал пом и убрал слжебные тесты

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,15 +2,12 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.Duration;
 
 @SpringBootApplication
 public class FilmorateApplication {
 
 	public static void main(String[] args) {
-		Film film1 = new Film("test1", "test1 ds", "2000.08.12", 120L);
 		SpringApplication.run(FilmorateApplication.class, args);
 	}
 
