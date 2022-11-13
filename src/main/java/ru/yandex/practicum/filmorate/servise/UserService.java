e67a0fb72bc746adff736874e7ed283e70cd9764
mkorovaynyy <corowainy@yandex.ru>
2022-11-13 23:59:17 +0300

    еще раз сиправил методы вывода друзей

--- src/main/java/ru/yandex/practicum/filmorate/servise/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/servise/UserService.java
@@ -6,6 +6,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 @Service
 public class UserService {
@@ -25,15 +26,12 @@ public class UserService {
         inMemoryUserStorage.getMapOfUser().get(idUser2).getFriends().remove(idUser1, inMemoryUserStorage.getMapOfUser().get(idUser1));
     }
     //вывод списка  друзей
-    public ArrayList<User> retListOfFriends(InMemoryUserStorage inMemoryUserStorage, Integer idUser) {
-        ArrayList<User> listOfFriends = new ArrayList<>();
-        for(User user: inMemoryUserStorage.getMapOfUser().get(idUser).getFriends().values()) {
-            listOfFriends.add(user);
-        }
-        return listOfFriends;
+    public Collection<User> retListOfFriends(InMemoryUserStorage inMemoryUserStorage, Integer idUser) {
+        return inMemoryUserStorage.getMapOfUser().get(idUser).getFriends().values();
     }
+
     //вывод списка общих друзей
-    public ArrayList<User> retListOfCommonFriends(InMemoryUserStorage inMemoryUserStorage, Integer idUser1, Integer idUser2) {
+    public Collection<User> retListOfCommonFriends(InMemoryUserStorage inMemoryUserStorage, Integer idUser1, Integer idUser2) {
         ArrayList<User> listOfFriends = new ArrayList<>();
         for(User user: inMemoryUserStorage.getMapOfUser().get(idUser1).getFriends().values()) {
             if (inMemoryUserStorage.getMapOfUser().get(idUser2).getFriends().containsKey(user.getId())) {
